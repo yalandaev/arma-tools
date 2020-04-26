bad2570480df33f06e6ea000094d5e64f0095123
Eugene Yalandaev <yalandaev@gmail.com>
2020-04-26 22:13:33 +0400

    TextureType enum

---
+++ Y.ArmaTools.Abstractions/TextureType.cs
@@ -0,0 +1,14 @@
+namespace Y.ArmaTools.Abstractions
+{
+    public enum TextureType
+    {
+        Co,
+        Ca,
+        Nohq,
+        Smdi,
+        As,
+        Ads,
+        Adshq,
+        Mc
+    }
+}
